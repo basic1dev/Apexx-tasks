ee056eeda90f73284fdbf460a1662052c97d60aa
basic1dev <jeromegennings@gmail.com>
2024-08-02 21:45:23 +0530

    Finalized the login and register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,10 @@
+@if ($errors->any())
+    <ul>
+        {!! implode('', $errors->all('<li>:message</li>')) !!}
+    </ul>
+@endif
+
+
 <form method="POST" action="authenticate">
     <label for="">email<input type="text" name="email"> </label>
     <label for="">password<input type="password" name="password"> </label>
